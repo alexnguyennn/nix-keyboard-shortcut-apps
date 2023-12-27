fb14f0d06cc59242d33fbcdc0e2749d0f2877fd9
Alex Nguyen <alex.nguyen@proton.me>
2023-12-27 21:43:48 +1100

    hardcode hid driver url

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,7 +2,6 @@ inputs: pkgs:
 let inherit (pkgs) callPackage; in
 {} // pkgs.lib.optionalAttrs pkgs.stdenv.isDarwin {
   Karabiner-DriverKit-VirtualHIDDevice = callPackage
-    ./darwin/Karabiner-DriverKit-VirtualHIDDevice
-    { Karabiner-DriverKit-VirtualHIDDevice-src = inputs.kmonad + "/../c_src/mac/Karabiner-DriverKit-VirtualHIDDevice"; };
+    ./darwin/Karabiner-DriverKit-VirtualHIDDevice {};
   kmonad-daemon-shim = callPackage ./darwin/kmonad-daemon-shim { };
 }
