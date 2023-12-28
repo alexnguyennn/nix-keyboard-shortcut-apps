d0f599007c7ea6d26a187922448adc281ff12c0b
Alex Nguyen <alex.nguyen@proton.me>
2023-12-28 19:19:20 +1100

    Revert "hardcode hid driver url"
    This reverts commit fb14f0d06cc59242d33fbcdc0e2749d0f2877fd9.
    
    Seems like the offending breaking code was overriding the `kmonad` package
    in ./pkgs/default.nix

--- pkgs/darwin/Karabiner-DriverKit-VirtualHIDDevice/default.nix
+++ pkgs/darwin/Karabiner-DriverKit-VirtualHIDDevice/default.nix
@@ -1,11 +1,13 @@
-{ pkgs, cpio, stdenv, xar }:
+{ Karabiner-DriverKit-VirtualHIDDevice-src, pkgs, cpio, stdenv, xar }:
 
 stdenv.mkDerivation {
   pname = "Karabiner-DriverKit-VirtualHIDDevice";
   version = "1.15.0";
   # use /raw/main/dist/* from filetree
   src = pkgs.fetchurl {
-    url = "https://github.com/pqrs-org/Karabiner-DriverKit-VirtualHIDDevice/raw/main/dist/Karabiner-DriverKit-VirtualHIDDevice-1.15.0.pkg";
+    # full path: "https://github.com/pqrs-org/Karabiner-DriverKit-VirtualHIDDevice/raw/main/dist/Karabiner-DriverKit-VirtualHIDDevice-1.15.0.pkg";
+    url =
+      "${Karabiner-DriverKit-VirtualHIDDevice-src}/dist/Karabiner-DriverKit-VirtualHIDDevice-1.15.0.pkg";
     sha256 = "sha256-Dkrnsc8waLs6le9k5aVSMKhL9KXm8FV47lnG/vZo4zE=";
 
   };
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,6 +2,7 @@ inputs: pkgs:
 let inherit (pkgs) callPackage; in
 {} // pkgs.lib.optionalAttrs pkgs.stdenv.isDarwin {
   Karabiner-DriverKit-VirtualHIDDevice = callPackage
-    ./darwin/Karabiner-DriverKit-VirtualHIDDevice {};
+    ./darwin/Karabiner-DriverKit-VirtualHIDDevice
+    { Karabiner-DriverKit-VirtualHIDDevice-src = inputs.kmonad + "/../c_src/mac/Karabiner-DriverKit-VirtualHIDDevice"; };
   kmonad-daemon-shim = callPackage ./darwin/kmonad-daemon-shim { };
 }
