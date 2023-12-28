78bae663e0a96e0044519c8e5d3b5a51a60510ab
Alex Nguyen <alex.nguyen@proton.me>
2023-12-28 18:32:00 +1100

    add kmonad overlays attribute

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,7 @@
 inputs: pkgs:
 let inherit (pkgs) callPackage; in
 {} // pkgs.lib.optionalAttrs pkgs.stdenv.isDarwin {
+  kmonad = inputs.kmonad.overlays.default;
   Karabiner-DriverKit-VirtualHIDDevice = callPackage
     ./darwin/Karabiner-DriverKit-VirtualHIDDevice
     { Karabiner-DriverKit-VirtualHIDDevice-src = inputs.kmonad + "/../c_src/mac/Karabiner-DriverKit-VirtualHIDDevice"; };
