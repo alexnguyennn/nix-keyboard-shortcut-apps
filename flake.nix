1d6fa1f059c1c9d614de3c45fab29c8e221c6586
Alex Nguyen <alex.nguyen@proton.me>
2025-06-11 22:59:46 +1000

    fix: make karabiner activate account for kanata
    * checks if service exists and the path exists
      with the latest driver ext we're checking for

--- flake.nix
+++ flake.nix
@@ -1,21 +1,27 @@
 {
-  # heavily ported from
-  description = "KMonad nix flake";
+  description =
+    "Nix flake set up that enables packages and system services that enable advanced keyboard shortcut configuration e.g. Kanata or KMonad";
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-
     kmonad = {
       url = "git+https://github.com/kmonad/kmonad?submodules=1&dir=nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    darwinModule = import ./modules/darwin/kmonad-and-kanata.nix;
-    overlays.default = nixpkgs.lib.composeManyExtensions [
-      inputs.kmonad.overlays.default
-      (_: prev: let pkgs = prev; in import ./pkgs inputs pkgs)
-    ];
-  };
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      driverKitExtVersion = "5.0.0";
+    in {
+      darwinModule = import ./modules/darwin/kmonad-and-kanata.nix {
+        inherit driverKitExtVersion;
+      };
+      overlays.default = nixpkgs.lib.composeManyExtensions [
+        inputs.kmonad.overlays.default
+        (_: prev:
+          let pkgs = prev;
+          in import ./pkgs inputs pkgs { inherit driverKitExtVersion; })
+      ];
+    };
 }
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,4 @@
-inputs: pkgs:
+inputs: pkgs: { driverKitExtVersion ? "5.0.0" }:
 let
   inherit (pkgs) callPackage;
   # Define the base URL and version for easy updates
@@ -7,6 +7,7 @@ in { } // pkgs.lib.optionalAttrs pkgs.stdenv.isDarwin {
     callPackage ./darwin/Karabiner-DriverKit-VirtualHIDDevice {
       Karabiner-DriverKit-VirtualHIDDevice-src = inputs.kmonad
         + "/../c_src/mac/Karabiner-DriverKit-VirtualHIDDevice";
+      inherit driverKitExtVersion;
     };
   karabiner-daemon-shim = callPackage ./darwin/karabiner-daemon-shim { };
 }
